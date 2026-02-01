a25ed1cab504827bbd3a8dbeec1fa59c1f34da79
Shubhankar <shubhankar.bhanot@unicorntechmedia.com>
2026-02-01 20:19:55 +0530

    set up redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,25 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import store from "./store/store";
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import { Provider } from "react-redux";
 
 function App() {
 	return (
 		<>
-			<BrowserRouter basename="/">
-				<Routes>
-					<Route path="/" element={<Body />}>
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<Signup />} />
-						<Route path="/about" element={<div>About Page</div>} />
-						<Route path="/profile" element={<div>Profile Page</div>} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+			<Provider store={store}>
+				<BrowserRouter basename="/">
+					<Routes>
+						<Route path="/" element={<Body />}>
+							<Route path="/login" element={<Login />} />
+							<Route path="/signup" element={<Signup />} />
+							<Route path="/about" element={<div>About Page</div>} />
+							<Route path="/profile" element={<div>Profile Page</div>} />
+						</Route>
+					</Routes>
+				</BrowserRouter>
+			</Provider>
 		</>
 	);
 }
