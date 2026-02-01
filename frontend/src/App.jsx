69f617ad63218fdc0f3d96a9afd665845c7cb375
Shubhankar <shubhankar.bhanot@unicorntechmedia.com>
2026-02-01 15:27:57 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
 
 function App() {
-
-  return (
-    <>
-    <div>
-      <h1>Hello</h1>
-    </div>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter basename="/">
+				<Routes>
+					<Route path="/" element={<Body />}>
+						<Route path="/login" element={<div>Login Page</div>} />
+						<Route path="/about" element={<div>About Page</div>} />
+						<Route path="/profile" element={<div>Profile Page</div>} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
